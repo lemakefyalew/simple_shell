1c4fe1504a6c88a950509b35479c6af2c2178dc0
Lema kefyalew <lmmbiz2002@gmail.com>
2022-05-18 16:47:02 +0300

    exit

--- exit.c
+++ exit.c
@@ -1,14 +1,5 @@
-#include "shell.h"
-
-/**
- * _exit_func - Function to exit successfully
- *
- * @environ: Environment variables.
- */
-
-void _exit_func(char **environ)
-{
-	(void)environ;
-	/* free_listint(*history); */
-	exit(98);
-}
+exit: exit [n]
+    Exit the shell.
+    
+    Exits the shell with a status of N.  If N is omitted, the exit status
+    is that of the last command executed.
